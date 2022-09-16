deb35982dc07f9fa40dde410a8708f7968b8075c
JesusGH123 <a01423730@itesm.mx>
2022-09-16 13:02:57 -0500

    Symbol table finished

--- KotlinAnalyzer.py
+++ KotlinAnalyzer.py
@@ -5,43 +5,57 @@ import SymbolTable
 
 #Variables
 skippedCharacters = SymbolTable.skippedCharacters
-delimiters = SymbolTable.delimiters
+separators = SymbolTable.separators
 reservedWords = SymbolTable.reservedWords
 readedTokens = []
 
 def readFile():
     global file
-    with open(sys.argv[1]) as f:
-        file = f.readlines()
-    
+    with open("Code.txt") as f:
+        file = f.readlines() 
 
 def analize():
+    delimitedComment = False
+    
     for line in file:
         currToken = ""
-        if(line.startswith("//") == False):  #Ignore one-line comments
-            for i in range(0, len(line)-1):
+        for i in range(0, len(line)):
+            if(line[i] == '/' and line[i+1] == '/'): #Skip lines with one-line comment
+                break;
+            elif(delimitedComment == False and line[i] == '/' and line[i+1] == '*'):  #Begginning of a multi-line comment
+                delimitedComment = True
+            elif(delimitedComment == True and line[i-1] == '*' and line[i] == '/'):  #End of multi-line comment
+                delimitedComment = False
+            elif(delimitedComment == False):        #Begin lexical check
                 if(line[i] in skippedCharacters):   #Check for skipping tokens
                     if(currToken != ""):
                         readedTokens.append(currToken)
                         currToken = ""
                     continue
-                elif(line[i] in delimiters):     #Check for delimiters
+                elif(line[i] in separators):     #Check for separators
                     if(currToken != ""):
                         readedTokens.append(currToken)
                         currToken = ""
                     readedTokens.append(line[i])
                 else:
                     currToken = currToken + line[i]
-                    
 
 def printTokens():
+    isAString = False
+
     for i in range(0, len(readedTokens)):
-        if(readedTokens[i] in delimiters):
-            print(readedTokens[i], delimiters[readedTokens[i]])
-        elif(readedTokens[i] in reservedWords):
+        if (readedTokens[i] == ('"' or "'")):
+            isAString = not isAString
+            print(readedTokens[i], "QUOTATION_MARK")
+        elif((readedTokens[i] in separators) and isAString == False):   #Print separators
+            print(readedTokens[i], separators[readedTokens[i]])
+        elif((readedTokens[i] in reservedWords) and isAString == False):    #Print reserved words
             print(readedTokens[i], reservedWords[readedTokens[i]])
+        elif(isAString == True):                                        #Print string literals
+            print(readedTokens[i], "LineString")
         else:
-            print("Not recognized character: ", readedTokens[i])
+            print(readedTokens[i], "ID")                                                   # All other characters are id's
+            #raise Exception("Not recognized character", readedTokens[i])
 
 # Driver code
 readFile()
--- SymbolTable.py
+++ SymbolTable.py
@@ -1,16 +1,43 @@
-skippedCharacters = [" ", "\t", "\n"]
+skippedCharacters = [" ", "\t", "\n", "\r\n", "\r"]
 
-delimiters = {
-    "(": "OPENING_PARENTHESIS",
-    ")": "CLOSING_PARENTHESIS",
-    ":": "DOUBLE_DOT",
-    "<": "OPENING_TYPE_BRACKET",
-    ">": "CLOSING_TYPE_BRACKET",
-    '"': "QUOTATION_MARK",
+separators = {
+    "#!": "SheBangLine", "[\u0020\u0009\u000C]": "WS",
+
+    #Separators and operations
+    "...": "RESERVED", ".": "DOT", ",": "COMMA",
+    "(": "LPAREN", ")": "RPAREN", "[": "LSQUARE", "]": "RSQUARE", "{": "LCURL", "}": "RCURL",
+    "*": "MULT", "%": "MOD", "/": "DIV", "+": "ADD", "-": "SUB",
+    "++": "INCR", "--": "DECR", "&&": "CONJ", "||": "DISJ", "!": "EXCL_WS",
+    ":": "COLON", ";": "SEMICOLON",
+    "=": "ASSIGNMENT", "+=": "ADD_ASSIGNMENT", "-=": "SUB_ASSIGNMENT", "*=": "MULT_ASSIGNMENT", "/=": "DIV_ASSIGNMENT", "%=": "MOD_ASSIGNMENT",
+    "->": "ARROW", "=>": "DOUBLE_ARROW", "..": "RANGE", "::": "COLONCOLON", ";;": "DOUBLE_SEMICOLON",
+    "#": "HASH", "@": "AT_NO_WS", "?": "QUEST_WS",
+    "<": "LANGLE", ">": "RANGLE", "<=": "LE", ">=": "GE",
+    "!=": "EXCL_EQ", "EXCL_EQEQ": "!==", "as?": "AS_SAFE", "==": "EQEQ", "===": "EQEQEQ",
+    "'": "SINGLE_QUOTE", '"': "QUOTE_OPEN",'"""': "TRIPLE_QUOTE_OPEN", "&": "AMP",
 }
 
 reservedWords = {
-    "println": "PRINT_FUNCTION",
-    "fun": "FUNCTION_DECLARATION",
-    "main": "FUNCTION_TYPE",
+    #Keywords
+    "return@": "RETURN_AT", "continue@": "CONTINUE_AT", "break@": "BREAK_AT", "this@": "THIS_AT", "super@": "SUPER_AT",
+    "file": "FILE", "field": "FIELD", "property": "PROPERTY", "get": "GET", "set": "SET", "receiver": "RECEIVER",
+    "param": "PARAM", "setparam": "SETPARAM", "delegate": "DELEGATE",
+    "package": "PACKAGE", "import": "IMPORT", "class": "CLASS", "interface": "INTERFACE", "fun": "FUN", "object": "OBJECT",
+    "val": "VAL", "var": "VAR", "typealias": "TYPE_ALIAS", "constructor": "CONSTRUCTOR", "by": "BY", "companion": "COMPANION",
+    "init": "INIT", "this": "THIS", "super": "SUPER", "typeof": "TYPEOF", "where": "WHERE", "if": "IF", "else": "ELSE", "when": "WHEN",
+    "try": "TRY", "catch": "CATCH", "finally": "FINALLY", "for": "FOR", "do": "DO", "while": "WHILE", "throw": "THROW", "return": "RETURN", "continue": "CONTINUE", "break": "BREAK",
+    "as": "AS", "is": "IS", "in": "IN", "!is": "NOT_IS", "!in": "NOT_IN", "out": "OUT", "dynamic": "dynamic",
+    #Lexical modifiers
+    "public": "PUBLIC", "private": "PRIVATE", "protected": "PROTECTED", "internal": "INTERNAL", "enum": "ENUM", "sealed": "SEALED", "annotation": "ANNOTATION", "data": "DATA", "inner": "INNER",
+    "value": "VALUE", "tailrec": "TAILREC", "operator": "OPERATOR", "inline": "INLINE", "infix": "INFIX", "external": "EXTERNAL", "suspend": "SUSPEND", "override": "OVERRIDE", "abstract": "ABSTRACT",
+    "final": "FINAL", "open": "OPEN", "const": "CONST", "lateinit": "LATEINIT", "vararg": "VARARG", "noinline": "NOINLINE", "crossonline": "CROSSINLINE", "reified": "REIFIED", "expect": "EXPECT", "actual": "ACTUAL",
+    #Section: Literals
+    "0": "DecDigit", "1": "DecDigit", "2": "DecDigit", "3": "DecDigit", "4": "DecDigit", "5": "DecDigit", "6": "DecDigit", "7": "DecDigit", "8": "DecDigit", "9": "DecDigit",
+    "_": "Separator", "e": "Exponent", "E": "Exponent",
+    "true": "BooleanLiteral", "null": "NullLiteral", "\\": "CharacterLiteral", "$": "FieldIdentifier",
+    "${": "LineExprStart",
+
+    #Extras
+    "Array": "ARRAY", "ListOf": "LIST_TYPE", "List": "LIST",
+    "String": "DATA_TYPE", "Float": "DATA_TYPE", "Int": "DATA_TYPE", "Char": "DATA_TYPE"
 }
