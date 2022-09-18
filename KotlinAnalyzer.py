bd87445ee63fe9844a5f3d4e131c7eb133edccab
JesusGH123 <a01423730@itesm.mx>
2022-09-18 02:02:00 -0500

    Lexical analyzer uses grammar rules for variables

--- KotlinAnalyzer.py
+++ KotlinAnalyzer.py
@@ -3,6 +3,7 @@
 import sys
 import SymbolTable
 import colorama
+import re
 from colorama import Fore
 
 #Variables
@@ -77,26 +78,63 @@ def classifyTokens():
         elif(isAString and isAnInterpolation == False):       #Print strings
             print(readedTokens[i], "LineString")
         elif(isAString and isAnInterpolation):
-            tokenValidation(readedTokens[i])
+            #tokenValidation(readedTokens[i])
             print(readedTokens[i], "String interpolation")
         elif((readedTokens[i] in separators) and isAString == False):  #Print separators
             print(readedTokens[i], separators[readedTokens[i]])
         elif((readedTokens[i] in reservedWords) and isAString == False):    #Print reserved words
             print(readedTokens[i], reservedWords[readedTokens[i]])
         else:
-            tokenValidation(readedTokens[i])
-            print(readedTokens[i], "ID")
+            literalValidation(readedTokens[i], False)
 
+def literalValidation(token, secondLap):
+    if(re.match(r'((^[1-9]+(\_*[0-9]*)*[0-9]$)|(^[0-9]$))', token)):
+        print(token, "IntegerLiteral")
+    elif(re.match(r'^[0-9]*\.?[0-9]+[fF]$', token)):
+        print(token, "RealLiteral")
+    elif(re.match(r'^[0-9]*\.?[0-9]+([eE][\+|\-]?[0-9]+)?$', token)):
+        print(token, "RealLiteral")
+    elif(re.match(r'((^(0[xX])[0-9a-fA-F](\_*[0-9a-fA-F]*)*[0-9a-fA-F]$)|(^(0[xX])[0-9a-fA-F])$)', token)):
+        print(token, "HexLiteral")
+    elif(re.match(r'((^(0[bB])[01](\_*[01]*)*[01]$)|(^(0[bB])[01])$)', token)):
+        print(token, "BinLiteral")
+    else:
+        if(secondLap == False):
+            tokenValidation(token)
+        else:
+            return False
+        
 def searchError(IncorrectToken):
-    for line in range(0, len(file)):
+    for line in range(0, len(file)):    #Search the error in the code
         if IncorrectToken in file[line]:
             return str(line+1)  
 
 def tokenValidation(token):
-    for character in token:
-                if(character not in alphabet):
-                    print(Fore.RED + "ERROR: Non recognized character at line " + searchError(token) + Fore.WHITE)
-                    exit(-1)
+    splitters = ['+','-','.']
+    splittedToken = []
+    currToken = ""
+
+    for i in range(0, len(token)+1):
+        if(i<len(token) and token[i] in splitters):
+            splittedToken.append(currToken)
+            splittedToken.append(token[i])
+            currToken = ""
+        elif(i == len(token)):
+            splittedToken.append(currToken)
+        else:
+            currToken += token[i]
+    
+    for token in splittedToken:
+        for character in token:         #Validate the token
+            if(character not in alphabet and
+                character not in reservedWords and
+                character not in separators):
+                print(Fore.RED + "ERROR: Non recognized character at line " + searchError(token) + Fore.WHITE)
+                exit(-1)
+        if(token in reservedWords):
+            print(token, reservedWords[token])
+        else:
+            print(token, "ID")
 
 # Driver code
 readFile()
--- SymbolTable.py
+++ SymbolTable.py
@@ -4,9 +4,10 @@ separators = {
     "#!": "SheBangLine",
 
     #Separators and operations
-    "...": "RESERVED", ".": "DOT", ",": "COMMA",
+    #".": "DOT",
+     "...": "RESERVED", ",": "COMMA",
     "(": "LPAREN", ")": "RPAREN", "[": "LSQUARE", "]": "RSQUARE", "{": "LCURL", "}": "RCURL",
-    "*": "MULT", "%": "MOD", "/": "DIV", "+": "ADD", "-": "SUB",
+    "*": "MULT", "%": "MOD", "/": "DIV",
     "++": "INCR", "--": "DECR", "&&": "CONJ", "||": "DISJ", "!": "EXCL_WS",
     ":": "COLON", ";": "SEMICOLON",
     "=": "ASSIGNMENT", "+=": "ADD_ASSIGNMENT", "-=": "SUB_ASSIGNMENT", "*=": "MULT_ASSIGNMENT", "/=": "DIV_ASSIGNMENT", "%=": "MOD_ASSIGNMENT",
@@ -15,8 +16,6 @@ separators = {
     "<": "LANGLE", ">": "RANGLE", "<=": "LE", ">=": "GE",
     "!=": "EXCL_EQ", "EXCL_EQEQ": "!==", "as?": "AS_SAFE", "==": "EQEQ", "===": "EQEQEQ",
     "'": "SINGLE_QUOTE", '"': "QUOTE_OPEN",'"""': "TRIPLE_QUOTE_OPEN", "&": "AMP", "${": "LineExprStart",
-
-    "0": "DecDigit", "1": "DecDigit", "2": "DecDigit", "3": "DecDigit", "4": "DecDigit", "5": "DecDigit", "6": "DecDigit", "7": "DecDigit", "8": "DecDigit", "9": "DecDigit",
 }
 
 reservedWords = {
@@ -27,20 +26,21 @@ reservedWords = {
     "package": "PACKAGE", "import": "IMPORT", "class": "CLASS", "interface": "INTERFACE", "fun": "FUN", "object": "OBJECT",
     "val": "VAL", "var": "VAR", "typealias": "TYPE_ALIAS", "constructor": "CONSTRUCTOR", "by": "BY", "companion": "COMPANION",
     "init": "INIT", "this": "THIS", "super": "SUPER", "typeof": "TYPEOF", "where": "WHERE", "if": "IF", "else": "ELSE", "when": "WHEN",
-    "try": "TRY", "catch": "CATCH", "finally": "FINALLY", "for": "FOR", "do": "DO", "while": "WHILE", "throw": "THROW", "return": "RETURN", "continue": "CONTINUE", "break": "BREAK",
-    "as": "AS", "is": "IS", "in": "IN", "!is": "NOT_IS", "!in": "NOT_IN", "out": "OUT", "dynamic": "dynamic", "IT": "it",
+    "try": "TRY", "catch": "CATCH", "finally": "FINALLY", "for": "FOR", "forEach": "FOR_EACH", "do": "DO", "while": "WHILE", "throw": "THROW", "return": "RETURN", "continue": "CONTINUE", "break": "BREAK",
+    "as": "AS", "is": "IS", "in": "IN", "!is": "NOT_IS", "!in": "NOT_IN", "out": "OUT", "dynamic": "dynamic", "it": "IT",
     #Lexical modifiers
     "public": "PUBLIC", "private": "PRIVATE", "protected": "PROTECTED", "internal": "INTERNAL", "enum": "ENUM", "sealed": "SEALED", "annotation": "ANNOTATION", "data": "DATA", "inner": "INNER",
     "value": "VALUE", "tailrec": "TAILREC", "operator": "OPERATOR", "inline": "INLINE", "infix": "INFIX", "external": "EXTERNAL", "suspend": "SUSPEND", "override": "OVERRIDE", "abstract": "ABSTRACT",
     "final": "FINAL", "open": "OPEN", "const": "CONST", "lateinit": "LATEINIT", "vararg": "VARARG", "noinline": "NOINLINE", "crossonline": "CROSSINLINE", "reified": "REIFIED", "expect": "EXPECT", "actual": "ACTUAL",
     #Section: Literals
-    "_": "Separator", "e": "Exponent", "E": "Exponent",
     "true": "BooleanLiteral", "false": "BooleanLiteral", "null": "NullLiteral", "\\": "CharacterLiteral", "$": "FieldIdentifier",
 
     #Extras
+    ".": "DOT", "+": "ADD", "-": "SUB",
     "Array": "ARRAY", "ListOf": "LIST_TYPE", "MutableList": "mutableListOf", "SET_TYPE": "setOf",  "MAP_TYPE": "mapOf", "MUTABLE_MAP": "mutableMapOf",
     "String": "DATA_TYPE", "Float": "DATA_TYPE", "Int": "DATA_TYPE", "Char": "DATA_TYPE", "Double": "DATA_TYPE", "Boolean": "DATA_TYPE", "Byte": "DATA_TYPE", "Short": "DATA_TYPE", "Long": "DATA_TYPE",
 }
 
 alphabet = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',
-            'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+            'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',
+            '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',]
